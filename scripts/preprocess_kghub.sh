2a1eff1267e5ca45c3b44d1b37a19f064c865fc7
Shawn O'Neil <shawn@tislab.org>
2024-01-23 07:30:35 +0000

    minor cleanup

--- scripts/preprocess_kghub.sh
+++ scripts/preprocess_kghub.sh
@@ -90,16 +90,3 @@ python3 -u kgedges_tsv_to_train_val_test_json.py \
 
 exit 0
 
-
-
-TASK="WN18RR"
-if [[ $# -ge 1 ]]; then
-    TASK=$1
-    shift
-fi
-
-python3 -u preprocess.py \
---task "${TASK}" \
---train-path "./data/${TASK}/train.txt" \
---valid-path "./data/${TASK}/valid.txt" \
---test-path "./data/${TASK}/test.txt"
